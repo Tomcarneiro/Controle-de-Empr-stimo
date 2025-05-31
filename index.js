6dff7d0b68447b4b8c87a87d4d8d1601e5f1436d
Tomcarneiro <dr.thompson.carneiro@gmail.com>
2025-05-31 12:54:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EmprestimosApp from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<EmprestimosApp />);
